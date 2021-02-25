605956629c23c5de35a84f51f019ead1dada77e6
leefuyong <tingting521>
2021-02-25 09:31:25 +0800

    c

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import INDEX from '@/views/index'
 import ARTICLE from '@/views/article'
+import LOGIN from '@/views/login'
 
 Vue.use(Router)
 
@@ -12,6 +13,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/login',
+      name: 'LOGIN',
+      component: LOGIN
     },{
       path: '/article',
       name: 'ARTICLE',
---
+++ src/views/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-row>
+
+    <a-card>
+
+      <a-form  :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" >
+        <a-form-item label="登录名称">
+          <a-input
+          />
+        </a-form-item>
+        <a-form-item label="登录密码">
+          <a-input
+          />
+        </a-form-item>
+        <a-form-item label=" ">
+          <a-button type="primary">确定登录</a-button>
+        </a-form-item>
+      </a-form>
+
+    </a-card>
+
+  </a-row>
+</template>
+
+<script>
+  export default {
+    name: ""
+  }
+</script>
+
+<style scoped>
+
+</style>
